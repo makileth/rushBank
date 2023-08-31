2710480242c113beee4962a55832e3be8a94cd7d
makileth <focyfox@gmail.com>
2023-08-31 16:45:56 +0200

    fixing problems with hosting

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
-    outDir: 'modern-app', 
-  },
+    outDir: './test' //Added
+  }
 });
